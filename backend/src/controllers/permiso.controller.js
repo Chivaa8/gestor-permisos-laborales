ff75410f4c481f0af5ec10296f1ec7fb24c92ce5
Chivaa8 <adaw23ochiva@inslaferria.cat>
2026-03-25 19:24:47 +0100

    feat: aprobar y rechazar permisos (admin)

--- backend/src/controllers/permiso.controller.js
+++ backend/src/controllers/permiso.controller.js
@@ -1,17 +1,16 @@
 import Permiso from "../models/tipoPermiso.model";
 
 class permisoController {
-  constructor() {}
-
+  //constructor() {}
 
   crearPermiso = async (req, res) => {
     try {
-      const nuevoPermiso = { // para que mande esto especificamente sino mandaría cualquier cosa
+      const nuevoPermiso = {
+        // para que mande esto especificamente sino mandaría cualquier cosa
         ...req.body,
         estado: "pendiente",
-        fechaCreacion: new Date()
+        fechaCreacion: new Date(),
       };
-
 
       const permiso = await Permiso.create(nuevoPermiso);
       res.status(201).json(permiso);
@@ -33,59 +32,103 @@ class permisoController {
     }
   };
 
-  obtenerPermisoPorId = async (req, res) =>{
+  obtenerPermisoPorId = async (req, res) => {
     try {
-      const {id} = req.params;
+      const { id } = req.params;
 
       const permiso = await Permiso.findById(req.params.id)
         .populate("empId")
         .populate("tipo")
-        .populate("empTramitador")
+        .populate("empTramitador");
 
       if (!permiso) {
         return res.status(404).json({ error: "Permiso no encontrado" });
       }
 
-      res.json(permiso)
-    } catch (error) {
-      res.status(500).json({ error: error.message})
-    }
-  }
-
-  retirarPermisos = async (req, res) => {
-    try {
-      await Permiso.findByIdAndDelete(req.params.id);
-      res.json({message: "Permiso eliminado"});
+      res.json(permiso);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   };
 
-  eliminarTodosPermisos = async (req, res) => { // esto solo lo podra hacer el admin
+  retirarPermisos = async (req, res) => {
+    try {
+      await Permiso.findByIdAndDelete(req.params.id);
+      res.json({ message: "Permiso eliminado" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  eliminarTodosPermisos = async (req, res) => {
+    // esto solo lo podra hacer el admin
     try {
       await Permiso.deleteMany({});
       res.json({ message: "Todos los permisos eliminados" });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  }
+  };
 
   mirarPermisos = async (req, res) => {
     try {
-      const {id} = req.params;
+      const { id } = req.params;
 
-      const permisos = await Permiso.find({ empId: id})
+      const permisos = await Permiso.find({ empId: id })
         .populate("tipo")
-        .populate("empTramitador")
+        .populate("empTramitador");
 
       res.status(200).json(permisos);
-    
-
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  }
+  };
 
+  aprobarPermiso = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { adminId } = req.body;
+
+      const permiso = await Permiso.findById(id);
+
+      if (!permiso) {
+        return res.status(404).json({ error: "Permiso no encontrado" });
+      }
+
+      permiso.estado = "aprobado";
+      permiso.empTramitador = adminId;
+      permiso.fechaTramintacion = new Date();
+
+      await permiso.save();
+
+      res.json(permiso);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  rechazarPermiso = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { adminId } = req.body;
+
+      const permiso = await Permiso.findById(id);
+
+      if (!permiso) {
+        return res.status(404).json({ error: "Permiso no encontrado" });
+      }
+
+      permiso.estado = "rechazado";
+      permiso.empTramitador = adminId;
+      permiso.fechaTramintacion = new Date();
+
+      await permiso.save();
+
+      res.json(permiso);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 }
 
 export default new permisoController();
--- backend/src/routes/permisos.routes.js
+++ backend/src/routes/permisos.routes.js
@@ -3,21 +3,29 @@ const route = express.Router(); // debemos gestionar las rutas
 import permisoController from "../controllers/permiso.controller";
 
 // creamos permiso
-route.post("/crear", permisoController.crearPermiso); 
+route.post("/crear", permisoController.crearPermiso);
 
 // damos todos los permisos
-route.post("/", permisoController.obtenerPermisos); 
+route.post("/", permisoController.obtenerPermisos);
 
 // damos un permiso
 route.post("/:id", permisoController.obtenerPermisoPorId);
 
 // eliminamos un permiso
-route.delete("/:id", permisoController.retirarPermisos); 
+route.delete("/:id", permisoController.retirarPermisos);
 
 // eliminamos todos los permisos
 route.delete("/", permisoController.eliminarTodosPermisos); // en un futuro esto solo lo podrá hacer el admin
 
 // permisos del usuario
-route.get("/:id/misPermisos", permisoController.obtenerPermisos); 
+route.get("/:id/misPermisos", permisoController.obtenerPermisos);
+
+// aprobar permiso
+
+route.put("/:id/aprobado", permisoController.aprobarPermiso);
+
+// rechar permiso
+
+route.put("/:id/rechazado", permisoController.rechazarPermiso);
 
 export default route;
